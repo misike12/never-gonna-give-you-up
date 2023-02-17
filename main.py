2bc41fbe0ba42e3d1e69880b15b60e0d74f730db
misike12 <59843249+misike12@users.noreply.github.com>
2023-02-17 08:35:56 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ main.py
@@ -0,0 +1,224 @@
+def on_button_pressed_a():
+    music.play_tone(277, music.beat(BeatFraction.WHOLE))
+    basic.pause(200)
+    music.play_tone(311, music.beat(BeatFraction.WHOLE))
+    basic.pause(200)
+    music.play_tone(208, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(311, music.beat(BeatFraction.WHOLE))
+    basic.pause(200)
+    music.play_tone(349, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(415, music.beat(BeatFraction.QUARTER))
+    music.play_tone(370, music.beat(BeatFraction.QUARTER))
+    music.play_tone(349, music.beat(BeatFraction.QUARTER))
+    music.play_tone(311, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(311, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(208, music.beat(BeatFraction.DOUBLE))
+    music.play_tone(415, music.beat(BeatFraction.QUARTER))
+    music.play_tone(370, music.beat(BeatFraction.QUARTER))
+    music.play_tone(349, music.beat(BeatFraction.QUARTER))
+    music.play_tone(311, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.WHOLE))
+    basic.pause(200)
+    music.play_tone(311, music.beat(BeatFraction.WHOLE))
+    basic.pause(200)
+    music.play_tone(208, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(311, music.beat(BeatFraction.WHOLE))
+    basic.pause(200)
+    music.play_tone(349, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(415, music.beat(BeatFraction.QUARTER))
+    music.play_tone(370, music.beat(BeatFraction.QUARTER))
+    music.play_tone(349, music.beat(BeatFraction.QUARTER))
+    music.play_tone(311, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(311, music.beat(BeatFraction.WHOLE))
+    basic.pause(100)
+    music.play_tone(208, music.beat(BeatFraction.DOUBLE))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    basic.pause(200)
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    basic.pause(500)
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.WHOLE))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(277, music.beat(BeatFraction.DOUBLE))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.HALF))
+    music.play_tone(175, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.DOUBLE))
+    basic.pause(200)
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.WHOLE))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(233, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.play_tone(208, music.beat(BeatFraction.QUARTER))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(233, music.beat(BeatFraction.HALF))
+    music.play_tone(208, music.beat(BeatFraction.HALF))
+    music.play_tone(175, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.DOUBLE))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    for index in range(9999):
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+        """)
+        basic.show_leds("""
+            . . . . .
+                        . # # # .
+                        . # # # .
+                        . # # # .
+                        . . . . .
+        """)
+        basic.show_leds("""
+            # . . . #
+                        . # # # .
+                        . # # # .
+                        . # # # .
+                        # . . . #
+        """)
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+        basic.show_leds("""
+            # # . # #
+                        # # . # #
+                        . . . . .
+                        # # . # #
+                        # # . # #
+        """)
+        basic.show_leds("""
+            # . . . #
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        # . . . #
+        """)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+input.on_button_pressed(Button.B, on_button_pressed_b)
